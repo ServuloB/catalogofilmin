91380ad1b04bd09367896920dfa1e9b19d0ca2f1
Servulo <matheusservulo4@gmail.com>
2022-05-30 11:49:43 -0300

    if

--- api/src/controller/usuarioController.js
+++ api/src/controller/usuarioController.js
@@ -8,6 +8,8 @@ server.post('/usuario/login' , async (req, resp) =>{
         const {email,senha} = req.body;
         const resposta= await login(email, senha);
 
+        if(!resposta) throw new Error("credenciais inválidas");
+
         resp.send(resposta)
     } catch(err){
         resp.status(400).send({
