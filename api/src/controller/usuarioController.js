cd128142ef4cd1035804fd46e47da4d0cb0bbbf6
Servulo <matheusservulo4@gmail.com>
2022-05-30 11:47:00 -0300

    primeiro commit

---
+++ api/src/controller/usuarioController.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { login } from "../repository/usuarioRepository.js";
+
+const server= Router();
+
+server.post('/usuario/login' , async (req, resp) =>{
+    try{ 
+        const {email,senha} = req.body;
+        const resposta= await login(email, senha);
+
+        resp.send(resposta)
+    } catch(err){
+        resp.status(400).send({
+            erro: err.message
+        });
+    }
+})
+
+export default server;
